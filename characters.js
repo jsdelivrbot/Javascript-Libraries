9b10a8f189fb0ece80f19d431bf15f911c6d0340
Nikedi <niko.kataja@yahoo.com>
2018-06-01 13:23:38 +0200

    Creation

---
+++ characters.js
@@ -0,0 +1,39 @@
+var lettersEn = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lettersEnCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lettersFi = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"];
+var lettersFiCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä", "Ö"];
+var lettersDe = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ä", "ö", "ü", "ß"];
+var lettersDeCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ä", "Ö", "Ü", "S", "S"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+makeArray("A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, Ä, Ö, Ü, ß", true, false)
+
+function makeArray(text, isArray, toUpper) {
+    text = text.split(", ").join("")
+    text = text.split(" ").join("");
+
+    if (toUpper === true) {
+        text = text.toUpperCase(text)
+    } else {
+        text = text.toLowerCase(text)
+    }
+
+    if (isArray === true) {
+        finalText = `["${text.split("").join('","')}"]`
+    } else {
+        finalText = `"${text.split("").join('","')}"`
+    }
+    console.log(finalText)
+}
