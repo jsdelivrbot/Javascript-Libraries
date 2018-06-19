b1ab7d08c878ca5f75958446a0ba7fb2a2656d46
Nikedi <niko.kataja@yahoo.com>
2018-06-19 20:17:59 +0200

    p

--- characters/characters.js
+++ characters/characters.js
@@ -1,24 +1,57 @@
 var lettersEn = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var lettersEnCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lettersFi = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"];
-var lettersFiCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä", "Ö"];
-var lettersDe = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ä", "ö", "ü", "ß"];
-var lettersDeCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ä", "Ö", "Ü", "ß"];
-var currencys = ["¤","؋","a","r","₿","฿","b","/",".","b","r","b","s",".","b","s",".","f",".","g","h","₵","¢","c","c","h",".","₡","c","$","d","ð","д","е","н","د","ج",".","د",".","ب","د",".","ع","j","d","د",".","ك","ل",".","د","д","и","н","د",".","ت","د",".","م",".","د",".","إ","d","b","$","₫","e","s","c","€","ƒ","f","t","f","b","u","f","c","f","a","c","f","a","f","r","f","r","w","g","g","r","₲","h","₴","₭","k","č","k","r","k","n","m","k","z","k","k","z","k","l","l","e","л","в",".","e","l","p","m","k","m","m","t","₥","n","f","k","₦","n","u",".","u","m","t","$","m","o","p","$","₱","p","t","£","l","l","l","s","p","q","q","r","r","$","ر",".","ع",".","ر",".","ق","ر",".","س","ر",".","ي","៛","r","m","p","₽","r","f",".","₹","₨","s","r","e","r","p","₪","t","s","h","k","s","h","s","h",".","s","o",".","u","s","h","s","/","s","d","r","с","о","м","৳","w","s","$","₮","v","t","₩","¥","z","ł"];
+var lettersEnShuffle = ["h","s","r","v","e","a","b","d","t","q","m","k","z","n","x","w","y","o","c","f","l","j","g","u","i","p"];
+var lettersEnCapsShuffle = ["Q","U","N","H","S","C","J","O","Z","A","P","M","D","B","F","I","T","K","G","Y","W","X","L","R","E","V"];
 var lettersEnAll = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lettersEnAllShuffle = ["i","b","R","h","o","U","E","V","X","a","p","S","w","u","H","f","v","N","B","j","K","Q","n","d","T","I","x","L","M","c","Y","s","O","z","t","g","G","P","m","k","y","W","A","l","Z","C","q","J","F","D","e","r"];
+
+var lettersFi = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"];
+var lettersFiShuffle = ["m","g","i","r","w","v","u","x","h","c","a","z","t","å","b","f","ö","k","p","o","j","l","d","q","ä","e","s","n","y"];
+var lettersFiCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä", "Ö"];
+var lettersFiCapsShuffle = ["G","T","L","N","M","Å","X","B","U","Ä","Z","Y","Ö","E","H","P","R","C","J","A","K","S","I","Q","D","W","F","V","O"];
 var lettersFiAll = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä", "Ö"];
+var lettersFiAllShuffle = ["L","G","y","ä","i","ö","U","v","j","W","x","R","E","B","C","w","s","m","t","c","k","u","l","a","F","z","Ä","o","Z","e","r","Å","V","I","å","f","q","N","K","p","O","S","d","g","P","J","M","D","X","Y","b","n","H","T","Ö","h","Q","A"];
+
+var lettersDe = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ä", "ö", "ü", "ß"];
+var lettersDeShuffle = ["y","k","ä","l","a","r","x","f","z","w","ß","d","h","t","u","n","i","q","m","o","c","ü","v","p","s","g","b","j","e","ö"];
+var lettersDeCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ä", "Ö", "Ü", "ß"];
+var lettersDeCapsShuffle = ["N","D","P","J","V","S","T","A","R","Ö","B","O","Y","Z","ß","H","Ü","L","X","Ä","C","W","U","G","Q","F","K","M","I","E"];
 var lettersDeAll = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ä", "ö", "ü", "ß", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ä", "Ö", "Ü", "ß"];
+var lettersDeAllShuffle = ["P","X","S","Ü","ü","r","u","s","ß","i","D","ö","K","T","I","Z","M","G","O","g","b","A","j","R","Y","v","ä","p","o","t","w","y","C","J","Q","l","h","E","U","q","V","c","z","N","x","Ö","B","n","d","W","H","F","a","e","f","Ä","m","k","ß","L"];
+
+var currencys = ["¤","؋","a","r","₿","฿","b","/",".","b","r","b","s",".","b","s",".","f",".","g","h","₵","¢","c","c","h",".","₡","c","$","d","ð","д","е","н","د","ج",".","د",".","ب","د",".","ع","j","d","د",".","ك","ل",".","د","д","и","н","د",".","ت","د",".","م",".","د",".","إ","d","b","$","₫","e","s","c","€","ƒ","f","t","f","b","u","f","c","f","a","c","f","a","f","r","f","r","w","g","g","r","₲","h","₴","₭","k","č","k","r","k","n","m","k","z","k","k","z","k","l","l","e","л","в",".","e","l","p","m","k","m","m","t","₥","n","f","k","₦","n","u",".","u","m","t","$","m","o","p","$","₱","p","t","£","l","l","l","s","p","q","q","r","r","$","ر",".","ع",".","ر",".","ق","ر",".","س","ر",".","ي","៛","r","m","p","₽","r","f",".","₹","₨","s","r","e","r","p","₪","t","s","h","k","s","h","s","h",".","s","o",".","u","s","h","s","/","s","d","r","с","о","м","৳","w","s","$","₮","v","t","₩","¥","z","ł"];
 
 
 
 
+shuffle(lettersDeAll) 
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  backThing(array)
+ // console.log(array)
+  return array;
+}
 
 
-
-
-
-
-
+function backThing(data) {
+  data = JSON.stringify(data) 
+  data = data.split("\n").join("");
+  console.log(data)
+}
 
 
 /*
